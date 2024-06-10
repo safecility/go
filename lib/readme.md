b05cf7ff4958c23fedd8cb76e7d2ab9cc6dbf843
tim <tim@theconvexlens.co>
2024-06-10 16:26:32 +0100

    update device meta and group information to UIDs

--- lib/readme.md
+++ lib/readme.md
@@ -2,6 +2,8 @@
 
 We define a basic microservice message format
 
+
+
 Our brokers can enhance this message but must retain the core structure for processing
 
 #### Helpers
--- lib/types.go
+++ lib/types.go
@@ -1,16 +1,36 @@
 package lib
 
-// Group our types use this slightly cumbersome naming convention for attributes to avoid erasure of values in datastore
-// and other mechanisms that flatten structures
-type Group struct {
-	GroupID       int64
-	GroupParentID int64
-	GroupChildren []Group
+// Device minimally identifies an IoT resource
+// a devices message typically identifies itself with a UID
+// the Meta information can then be accessed and added to the struct
+type Device struct {
+	DeviceUID string
+	*DeviceMeta
+	*Group
 }
 
-type Device struct {
-	DeviceUID  string
+// DeviceMeta helps further identify a Device.
+// Deployed devices typically have an UID that identifies the physical unit.
+// DeviceName is a human readable identifier for the device which may or may not be unique
+// DeviceTag identifies the function of the device in its environment and should remain constant is the device is
+// switched out
+// due to failure or replacement but the function of the device remains constant: so for example the cooler in room 4B is still
+// the cooler in 4B even if it is upgraded from PhilipsKA4500 to PhilipsKA4501
+type DeviceMeta struct {
 	DeviceName string
-	CompanyID  int64
-	*Group
+	DeviceTag  string
+}
+
+// Group provides Devices with a basic organizational system that can be easily cached and added as metadata early in a
+// processing pipeline
+// our types use this slightly cumbersome naming convention for attributes to avoid erasure of values in datastore
+// and other mechanisms that flatten structures
+// a Group always has a GroupUID and belongs to a Company - if the group is a top level group then the GroupParentUID is
+// the same as the CompanyUID or empty - group structures do not need to represent all or any of their children but may
+// choose to do so (children may instead name their ParentUID and associate in this way instead)
+type Group struct {
+	GroupUID       string
+	GroupParentUID string
+	GroupChildren  []Group
+	CompanyUID     string
 }
